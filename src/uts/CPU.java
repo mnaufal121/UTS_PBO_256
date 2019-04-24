46367282438b5f72fb5d1fdfc1fca91e68382da6
Asus <Asus@LAPTOP-M8G14NMJ>
2019-04-24 17:12:21 +0700

    Update kelas CPU beserta Kelas anaknya

--- src/uts/CPU.java
+++ src/uts/CPU.java
@@ -10,5 +10,30 @@ package uts;
  * @author Asus
  */
 public class CPU {
+        protected String nama;
+    protected int kecepatan;
     
+    public CPU(String nama, int kecepatan){
+        this.nama = nama;
+        this.kecepatan = kecepatan;
+    }
+    public CPU(){
+        
+    }
+    public String getCPUData(){
+        return nama+" "+kecepatan;
+    }
+}
+class intel extends CPU{    
+    public intel(int kecepatan) {
+      this.kecepatan = kecepatan;
+      this.nama="Intel";
+    }
+
+class amd extends CPU{
+    public amd(int kecepatan){
+        this.kecepatan = kecepatan;
+        this.nama = "AMD";
+    }
+}
 }
